3b53a9231f47b7d3a5ddb5ac0ab3a55530fda253
Danny <kiss5891@gmail.com>
2015-07-01 09:22:22 +0800

    update0701

---
+++ 4-01if.py
@@ -0,0 +1,10 @@
+
+
+inputInt=input()
+
+if inputInt==3:
+    print("3")
+elif inputInt==4:
+    print("4")
+else:
+    print("other")
---
+++ 4-02while.py
@@ -0,0 +1,13 @@
+a=3
+b=True
+while b:
+    print(a)
+    a+=1
+    b=False
+    #break
+
+else:
+    print("e")
+    
+    
+
